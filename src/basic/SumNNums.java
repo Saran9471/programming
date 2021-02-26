96dd46093102356d9086b51ffa627969f20fc004
SaRaN <saran9471@gmail.com>
2021-02-26 21:02:13 +0530

    sum of n nos commit

--- src/basic/SumNNums.java
+++ src/basic/SumNNums.java
@@ -1,8 +1,19 @@
 package basic;
 
+import java.util.*;
+
 public class SumNNums {
 
     public static void main(String[] args) {
-        // n nos
+        Scanner inp = new Scanner(System.in);
+        int n, sum = 0;
+        n = inp.nextInt();
+        System.out.println("Enter the " + n + " numbers");
+        for(int i = 0;i<n;i++)
+        {
+            int temp = inp.nextInt();
+            sum += temp;
+        }
+        System.out.println(sum);
     }
 }
